85bd8daf64bbba0be17432768c14652a55328cca
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-04-08 19:38:52 +0300

    Chore: clear logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app';
 
-import { helpers } from '@/utils/helpers';
-
 import '@/styles/globals.css';
 
 export const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,8 @@ import Head from 'next/head';
 
 import { strapiApi } from '@/src/utils/api';
 
-import styles from '@/styles/Home.module.css';
-
 export async function getStaticProps(_context) {
   const response = await strapiApi.request.get('/sneakers/1');
-  console.log('@@', response.data);
   return {
     props: { data: response.data.data },
   };
@@ -22,7 +19,7 @@ const Home = ({ data }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={styles.main}>
+      <main>
         <h1 className='p-4 pt-2 text-3xl font-bold underline '>
           Hello world!
           {data.attributes.shoe}
