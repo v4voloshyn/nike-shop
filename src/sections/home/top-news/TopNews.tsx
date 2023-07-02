a4b4e60346e82dd2aaf37060e9efea8dde233cc8
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-07-02 23:21:08 +0300

    feat: add video to TopNews

--- src/sections/home/top-news/TopNews.tsx
+++ src/sections/home/top-news/TopNews.tsx
@@ -6,8 +6,22 @@ import { Button, Typography } from '@/src/components';
 interface TopNewsProps {}
 
 export const TopNews: FC<TopNewsProps> = () => (
-  <section className='mt-[3rem]'>
-    <div className='align-end flex flex-col items-center text-center'>
+  <section className=''>
+    <div className=''>
+      <video
+        width='100%'
+        height='100%'
+        autoPlay
+        loop
+        muted
+        className='aspect-[2.88] min-h-[20vh] object-cover'
+        preload='auto'
+      >
+        <source src='/video/nike-full.mp4' type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div className='align-end mt-[3rem] flex flex-col items-center text-center'>
       <Typography tag='h1' variant='title-1'>
         LEADERS LIKE NEVER BEFORE
       </Typography>
